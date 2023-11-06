1a0f46937a3beca22af7608e8866ea83a628dd2d
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-11-06 11:19:44 -0500

    first commit

---
+++ next-ai-blog-app-main/app/(shared)/Other.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+
+type Props = {};
+
+const Other = (props: Props) => {
+  return (
+    <section className="pt-4 mb-16">
+      <hr className="border-1" />
+      {/* header */}
+      <div className="flex items-center gap-3 my-8">
+        <p className="font-bold text-2xl">Other Trending Posts</p>
+      </div>
+      <div className="sm:grid grid-cols-2 gap-16">
+        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80"></Card>
+        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80"></Card>
+        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80"></Card>
+        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80"></Card>
+      </div>
+    </section>
+  );
+};
+
+export default Other;
